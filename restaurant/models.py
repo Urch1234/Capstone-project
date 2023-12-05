6098cc0cab2b607d464c97f11b17b0930ff07564
Ihezie Ernest <iernest61@gmail.com>
2023-12-05 13:13:16 +0100

    added views, updated serializers and model

--- restaurant/models.py
+++ restaurant/models.py
@@ -4,9 +4,17 @@ from django.db import models
 class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guests = models.IntegerField(default=6)
-    bookingDate = models.DateTimeField()
+    booking_date = models.DateTimeField()
 
 class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.SmallIntegerField(default=5)
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+
+    def get_item(self):
+        return f"{self.title} : {self.price}"
--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,8 +1,24 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Menu
+from .models import Menu, Booking, MenuItem
 
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Memu
-        fields = 
+        model = Menu
+        fields = '__all__'
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Booking
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Menu:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = MenuItem
+        fields = '__all__'
