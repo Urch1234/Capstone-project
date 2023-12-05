d363d59caefc96a25ebffe61915692f2339d0cb6
Ihezie Ernest <iernest61@gmail.com>
2023-12-05 13:50:04 +0100

    updated urls at Project level, added viewset ran migrations

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import MenuItem
-from .serializers import MenuItemSerializer
+from rest_framework import permissions, viewsets
+from django.contrib.auth.models import User
+from .models import MenuItem, Booking
+from .serializers import MenuItemSerializer, UserSerializer, BookingSerializer
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, "index.html", {})
+
 
 class MenuItemView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
+
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
