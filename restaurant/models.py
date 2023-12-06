5ca26228e4fe6341a9b2b2fe290f08bcce57fbc8
Ihezie Ernest <iernest61@gmail.com>
2023-12-06 17:39:53 +0100

    api testing update

--- restaurant/models.py
+++ restaurant/models.py
@@ -2,19 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Booking(models.Model):
-    name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
     no_of_guests = models.IntegerField(default=6)
     booking_date = models.DateTimeField()
 
+    def __str__(self):
+        return self.first_name
+
 class Menu(models.Model):
-    title = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.SmallIntegerField(default=5)
+    menu_item_description = models.TextField(max_length=1000, default='')
+
+    def __self__(self):
+        return f'{self.name} : {str(self.price)}'
 
 class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.SmallIntegerField()
 
-    def get_item(self):
+    def __self__(self):
         return f"{self.title} : {self.price}"
